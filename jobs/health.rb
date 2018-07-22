e0ecebd1ecb03491cafac8ff00525ad2f8620b92
Murray Tait <death.tumble@gmail.com>
2018-07-22 18:53:07 +0100

    Use public url for healthz checks

--- jobs/health.rb
+++ jobs/health.rb
@@ -32,7 +32,7 @@ SCHEDULER.every '20s' do
       privateUriString = 'http://' + server["privateIpAddress"] + ':8082/healthz'
       publicUriString = 'http://' + server["publicIpAddress"] + ':8082/healthz'
 
-      uri = URI.parse(privateUriString)
+      uri = URI.parse(publicUriString)
       
       begin  
         http = Net::HTTP.new(uri.host, uri.port)
